00316914c4b7b372e6dba95aa40c693897ff9565
marvincwhuang <cwhuang0523@gmail.com>
2021-11-10 22:55:35 +0800

    update Web3Status

--- src/components/Header/Web3Status.tsx
+++ src/components/Header/Web3Status.tsx
@@ -1,8 +1,8 @@
 import { useMemo } from 'react'
 import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core'
 import { useTheme, Box, styled } from '@mui/material'
-import { CountUp } from 'use-count-up'
-import { Warning } from '@mui/icons-material'
+// import { CountUp } from 'use-count-up'
+// import { Activity } from 'react-feather'
 import Copy from 'components/essential/Copy'
 import { NetworkContextName } from '../../constants'
 import useENSName from '../../hooks/useENSName'
@@ -13,29 +13,82 @@ import { TransactionDetails } from '../../state/transactions/reducer'
 import { shortenAddress } from '../../utils'
 import WalletModal from 'components/Modal/WalletModal/index'
 import Spinner from 'components/Spinner'
-import { TokenAmount } from 'constants/token'
-import { useAggregateUniBalance } from 'state/wallet/hooks'
-import usePrevious from '../../hooks/usePrevious'
-import Divider from 'components/Divider'
-import useBreakpoint from 'hooks/useBreakpoint'
+// import { TokenAmount } from 'constants/token'
+// import { useAggregateUniBalance } from 'state/wallet/hooks'
+// import usePrevious from '../../hooks/usePrevious'
+// import Divider from 'components/Divider'
 import TextButton from 'components/Button/TextButton'
 import Button from 'components/Button/Button'
+// import { ReactComponent as UcenterIcon } from 'assets/svg/ucenter.svg'
+// import { useHistory } from 'react-router'
+// import { UserInfoTabRoute, UserInfoTabs } from 'pages/Account'
 
-const Dot = styled('span')({
-  width: 12,
-  height: 12,
-  background: `linear-gradient(135deg, #ffffff 4.17%, rgba(255, 255, 255, 0) 75%)`,
-  border: '0.6px solid #ffffff',
-  boxSizing: 'border-box',
-  borderRadius: '50%'
+const UserButtonWrap = styled('div')({
+  marginLeft: '5px',
+  position: 'relative',
+  width: 36,
+  height: 36,
+  '& > div': {
+    opacity: 0,
+    visibility: 'hidden'
+  },
+  '&:hover': {
+    '& > div': {
+      opacity: 1,
+      visibility: 'visible'
+    }
+  }
 })
 
+// const UserMenuWrapper = styled('div')({
+//   position: 'absolute',
+//   top: '50px',
+//   right: 0,
+//   zIndex: 2000,
+//   minWidth: '15rem',
+//   boxSizing: 'border-box',
+//   backgroundColor: '#ffffff',
+//   overflow: 'hidden',
+//   borderRadius: '16px',
+//   transitionDuration: '0.3s',
+//   transitionProperty: 'visibility, opacity',
+//   display: 'flex',
+//   border: '1px solid #ededed',
+//   flexDirection: 'column',
+//   '& > div:first-child': {
+//     padding: '16px 24px',
+//     display: 'flex',
+//     alignItems: 'center',
+//     borderBottom: '1px solid #ededed',
+//     width: '100%'
+//   },
+//   '& > button:last-child': {
+//     padding: '16px 24px',
+//     borderTop: '1px solid #ededed'
+//   }
+// })
+
+// const UserMenuItem = styled('button')({
+//   padding: '12px 24px',
+//   width: '100%',
+//   border: 'none',
+//   backgroundColor: 'transparent',
+//   textAlign: 'left',
+//   fontSize: '16px',
+//   cursor: 'pointer',
+//   '&:hover': {
+//     backgroundColor: '#ededed'
+//   }
+// })
+
 const ActionButton = styled(Button)(({ theme }) => ({
-  [theme.breakpoints.down('md')]: {
+  backgroundColor: theme.palette.error.main,
+  fontSize: '14px',
+  [theme.breakpoints.down('sm')]: {
     maxWidth: 320,
     width: '100%',
     borderRadius: 49,
-    height: 40
+    height: '40px'
   }
 }))
 
@@ -51,12 +104,11 @@ const SOCK = (
 )
 
 function Web3StatusInner() {
-  const { account, connector, error } = useWeb3React()
-  const isDownMD = useBreakpoint('md')
-  const aggregateBalance: TokenAmount | undefined = useAggregateUniBalance()
+  const { account, error } = useWeb3React()
+  // const aggregateBalance: TokenAmount | undefined = useAggregateUniBalance()
 
-  const countUpValue = aggregateBalance?.toFixed(0) ?? '0'
-  const countUpValuePrevious = usePrevious(countUpValue) ?? '0'
+  // const countUpValue = aggregateBalance?.toFixed(0) ?? '0'
+  // const countUpValuePrevious = usePrevious(countUpValue) ?? '0'
 
   const { ENSName } = useENSName(account ?? undefined)
 
@@ -69,6 +121,12 @@ function Web3StatusInner() {
 
   const pending = sortedRecentTransactions.filter(tx => !tx.receipt).map(tx => tx.hash)
 
+  // const history = useHistory()
+  // const toShowUserPanel = useCallback(() => {
+  //   history.push('/profile')
+  //   return
+  // }, [history])
+
   const hasPendingTransactions = !!pending.length
   const hasSocks = useHasSocks()
   const toggleWalletModal = useWalletModalToggle()
@@ -76,16 +134,20 @@ function Web3StatusInner() {
   if (account) {
     return (
       <Box
-        height={'32px'}
-        display={'flex'}
-        border={'1px solid #FFFFFF'}
-        borderRadius="4px"
-        alignItems={'center'}
-        style={{ fontSize: 14 }}
+        sx={{
+          height: '36px',
+          display: 'flex',
+          pl: '10px',
+          borderRadius: '32px',
+          fontSize: 14,
+          lineHeight: '16px',
+          background: 'rgba(255,255,255,0.1)',
+          alignItems: 'center'
+        }}
       >
-        {!!account && aggregateBalance && (
+        {/* {!!account && aggregateBalance && (
           <>
-            <Box padding={isDownMD ? '0 8px' : '0 10px 0 12px'} gap={10}>
+            <Box padding={isDownMD ? '0 8px' : '0 10px 0 12px'} gridGap={10}>
               <CountUp
                 key={countUpValue}
                 isCounting
@@ -99,42 +161,70 @@ function Web3StatusInner() {
             </Box>
             <Divider orientation={'vertical'} />
           </>
-        )}
-        <Box display="flex" alignItems="center" padding={isDownMD ? '0 8px' : '0 12px 0 10px'} gap={10}>
+        )} */}
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            padding: {
+              xs: '0 0 0 8px',
+              md: '0 0 0 10px'
+            },
+            gap: 10
+          }}
+        >
           {hasPendingTransactions ? (
-            <>
+            <Box
+              sx={{
+                margin: '0 auto',
+                gap: 10,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
               <Spinner color={theme.textColor.text1} size="16px" />
-              <span>{pending?.length} Pending</span>
-            </>
+              <Box
+                component="span"
+                sx={{
+                  mr: {
+                    xs: '8px',
+                    md: '10px'
+                  }
+                }}
+              >
+                {pending?.length} Pending
+              </Box>
+            </Box>
           ) : (
             <>
               {hasSocks ? SOCK : null}
-              {!hasPendingTransactions && connector && <Dot />}
-              <TextButton onClick={toggleWalletModal} fontSize={12} opacity={0.6}>
+              <TextButton onClick={toggleWalletModal} fontSize={14} opacity={0.5}>
                 {ENSName || shortenAddress(account)}
               </TextButton>
               {account && <Copy toCopy={account}></Copy>}
             </>
           )}
+          <UserButtonWrap>
+            {/* <UserButton id="userButton" onClick={toShowUserPanel} isOpen={!!match}>
+                  <AntimatterIcon />
+                </UserButton> */}
+            {/* <UcenterIcon onClick={toShowUserPanel} style={{ width: 36, position: 'absolute', cursor: 'pointer' }} /> */}
+            {/* <UserMenu account={account} /> */}
+          </UserButtonWrap>
         </Box>
       </Box>
     )
   } else if (error) {
     return (
-      <ActionButton
-        backgroundColor={theme.palette.error.main}
-        fontSize={'14px'}
-        width={'140px'}
-        height={'32px'}
-        onClick={toggleWalletModal}
-      >
-        <Warning sx={{ fontSize: 16, mr: 10 }} />
+      <ActionButton width="140px" height="36px" onClick={toggleWalletModal}>
+        {/* <Activity size={16} style={{ marginRight: 10 }} /> */}
         {error instanceof UnsupportedChainIdError ? 'Wrong Network' : 'Error'}
       </ActionButton>
     )
   } else {
     return (
-      <ActionButton fontSize={'14px'} width={'140px'} height={'32px'} onClick={toggleWalletModal}>
+      <ActionButton width="140px" height="36px" onClick={toggleWalletModal}>
         Connect Wallet
       </ActionButton>
     )
@@ -168,3 +258,29 @@ export default function Web3Status() {
     </>
   )
 }
+
+// function UserMenu({ account }: { account?: string | null }) {
+//   const toggleWalletModal = useWalletModalToggle()
+//   const history = useHistory()
+
+//   return (
+//     <UserMenuWrapper>
+//       <div>
+//         <UcenterIcon style={{ width: 24 }} />
+//         <TextButton fontSize={14} opacity={0.5} style={{ margin: '0 15px', color: '#000' }}>
+//           {account && shortenAddress(account)}
+//         </TextButton>
+//         {account && <Copy toCopy={account} />}
+//       </div>
+//       <Box display="flex" flexDirection="column">
+//         <UserMenuItem onClick={() => history.push('/profile/' + UserInfoTabs.INVENTORY)}>
+//           {UserInfoTabRoute[UserInfoTabs.INVENTORY]}
+//         </UserMenuItem>
+//         <UserMenuItem onClick={() => history.push('/profile/' + UserInfoTabs.ACTIVITY)}>
+//           {UserInfoTabRoute[UserInfoTabs.ACTIVITY]}
+//         </UserMenuItem>
+//         <UserMenuItem onClick={toggleWalletModal}>Wallet</UserMenuItem>
+//       </Box>
+//     </UserMenuWrapper>
+//   )
+// }
