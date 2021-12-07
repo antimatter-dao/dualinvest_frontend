2cce8e4a405d4d4dc43f9720a2e8e06cacee9262
yukamend <lukas.amend@rwth-aachen.de>
2021-12-08 00:05:34 +0100

    fix StatusTag

--- src/components/Status/StatusTag.tsx
+++ src/components/Status/StatusTag.tsx
@@ -1,46 +1,24 @@
 import { Box } from '@mui/material'
 
 interface Props {
-  status: string
+  status: 'progressing' | 'recruited' | 'completed' | 'failed'
 }
 
 export default function StatusTag(props: Props) {
   const { status } = props
-  let color, bgcolor, statustext
-
-  switch (status) {
-    case 'progressing':
-      statustext = 'Progressing'
-      color = '#18A0FB'
-      bgcolor = 'rgba(24, 160, 251, 0.16)'
-      break
-    case 'recruited':
-      statustext = 'Recruited'
-      color = '#31B047'
-      bgcolor = 'rgba(49, 176, 71, 0.16)'
-      break
-    case 'completed':
-      statustext = 'Completed'
-      color = '#11BF2D'
-      bgcolor = 'rgba(17, 191, 45, 0.16)'
-      break
-    case 'failed':
-      statustext = 'Failed'
-      color = '#FF0000'
-      bgcolor = 'rgba(255,3,3,0.16)'
-      break
-    default:
-      statustext = 'undefined'
-      color = '#000000'
-      bgcolor = 'rgba(0,0,0,0.16)'
+  const statusStyles = {
+    progressing: { statusText: 'Progressing', color: '#18A0FB', bgColor: 'rgba(24, 160, 251, 0.16)' },
+    recruited: { statusText: 'Recruited', color: '#31B047', bgColor: 'rgba(49, 176, 71, 0.16)' },
+    completed: { statusText: 'Completed', color: '#11BF2D', bgColor: 'rgba(17, 191, 45, 0.16)' },
+    failed: { statusText: 'Failed', color: '#FF0000', bgColor: 'rgba(255,3,3,0.16)' }
   }
 
   return (
     <Box
       component="div"
       borderRadius={22}
-      color={color}
-      bgcolor={bgcolor}
+      color={statusStyles[status].color}
+      bgcolor={statusStyles[status].bgColor}
       fontSize={14}
       display="flex"
       alignItems="center"
@@ -48,7 +26,7 @@ export default function StatusTag(props: Props) {
       width={100}
       height={36}
     >
-      {statustext}
+      {statusStyles[status].statusText}
     </Box>
   )
 }
