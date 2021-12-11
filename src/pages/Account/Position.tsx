5cc2a0815681a3b4a7877480be9eb244905c6184
marvincwhuang <cwhuang0523@gmail.com>
2021-12-12 01:06:24 +0800

    fix

--- src/pages/Account/Position.tsx
+++ src/pages/Account/Position.tsx
@@ -200,7 +200,7 @@ function PositionTableCards({ data }: { data: { summary: any[]; details: any[] }
               return (
                 <Box key={idx} display="flex" justifyContent="space-between">
                   <Typography fontSize={12} color="#000000" sx={{ opacity: 0.5 }}>
-                    {PositionTableHeader[idx]}
+                    {PositionTableHeader[idx2]}
                   </Typography>
                   <Typography fontSize={12} fontWeight={600}>
                     {datum}
