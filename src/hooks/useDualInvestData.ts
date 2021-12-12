d203485538f9ed55ef11340cfcdbf0214dcaa312
marvincwhuang <cwhuang0523@gmail.com>
2021-12-12 15:06:00 +0800

    add usePollingWithMaxRetries

--- src/hooks/useDualInvestData.ts
+++ src/hooks/useDualInvestData.ts
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Axios } from 'utils/axios'
 import { ProductList, productListFormatter, productFormatter, Product, OrderRecord } from 'utils/fetch/product'
 import { AccountRecord } from 'utils/fetch/account'
 import { useActiveWeb3React } from 'hooks'
+import usePollingWithMaxRetries from './usePollingWithMaxRetries'
 
 export enum InvestStatus {
   Confirming = 1,
@@ -16,48 +17,22 @@ export enum InvestStatus {
 export function useProductList() {
   const [productList, setProductList] = useState<ProductList | undefined>(undefined)
 
-  useEffect(() => {
-    const id = setInterval(() => {
-      Axios.get('getProducts')
-        .then(r => {
-          if (r.data.code !== 200) {
-            throw Error(r.data.msg)
-          }
-          setProductList(productListFormatter(r.data.data))
-        })
-        .catch(e => {
-          console.error(e)
-        })
-    }, 3000)
+  usePollingWithMaxRetries(
+    () => Axios.get('getProducts'),
+    r => setProductList(productListFormatter(r.data.data))
+  )
 
-    return () => {
-      clearInterval(id)
-    }
-  })
   return productList
 }
 
 export function useProduct(productId: string) {
   const [product, setProduct] = useState<Product | undefined>(undefined)
 
-  useEffect(() => {
-    const id = setInterval(() => {
-      Axios.get('getProducts?productId=' + productId)
-        .then(r => {
-          if (r.data.code !== 200) {
-            throw Error(r.data.msg)
-          }
-          setProduct(productFormatter(r.data.data))
-        })
-        .catch(e => {
-          console.error(e)
-        })
-    }, 3000)
+  usePollingWithMaxRetries(
+    () => Axios.get('getProducts?productId=' + productId),
+    r => setProduct(productFormatter(r.data.data))
+  )
 
-    return () => {
-      clearInterval(id)
-    }
-  })
   return product
 }
 
@@ -70,29 +45,18 @@ export function useAccountRecord(pageNum = 1, pageSize = 8) {
     total: 0
   })
 
-  useEffect(() => {
-    const id = setInterval(() => {
-      Axios.get('getAccountRecord', { address: account, pageNum, pageSize })
-        .then(r => {
-          if (r.data.code !== 200) {
-            throw Error(r.data.msg)
-          }
-          setAccountRecord(r.data.data)
-          setPageParams({
-            count: parseInt(r.data.data.pages, 10),
-            perPage: parseInt(r.data.data.size, 10),
-            total: parseInt(r.data.data.total, 10)
-          })
-        })
-        .catch(e => {
-          console.error(e)
-        })
-    }, 3000)
-
-    return () => {
-      clearInterval(id)
+  usePollingWithMaxRetries(
+    () => Axios.get('getAccountRecord', { address: account, pageNum, pageSize }),
+    r => {
+      setAccountRecord(r.data.data)
+      setPageParams({
+        count: parseInt(r.data.data.pages, 10),
+        perPage: parseInt(r.data.data.size, 10),
+        total: parseInt(r.data.data.total, 10)
+      })
     }
-  }, [account, pageNum, pageSize])
+  )
+
   return { accountRecord, pageParams }
 }
 
@@ -105,34 +69,26 @@ export function useOrderRecords(investStatus?: number, pageNum?: number, pageSiz
     total: 0
   })
 
-  useEffect(() => {
-    const id = setInterval(() => {
-      Axios.get<{ records: OrderRecord[]; pages: string; size: string; total: string }>('getOrderRecord', {
-        address: account,
+  console.log(account)
+
+  usePollingWithMaxRetries(
+    () =>
+      Axios.get<{ records: OrderRecord[]; pages: string; size: string; total: string }>('getOrderRecords', {
+        address: null,
         investStatus,
         pageNum,
         pageSize
+      }),
+    r => {
+      setOrderList(r.data.data.records)
+      setPageParams({
+        count: parseInt(r.data.data.pages, 10),
+        perPage: parseInt(r.data.data.size, 10),
+        total: parseInt(r.data.data.total, 10)
       })
-        .then(r => {
-          if (r.data.code !== 200) {
-            throw Error(r.data.msg)
-          }
-          setOrderList(r.data.data.records)
-          setPageParams({
-            count: parseInt(r.data.data.pages, 10),
-            perPage: parseInt(r.data.data.size, 10),
-            total: parseInt(r.data.data.total, 10)
-          })
-        })
-        .catch(e => {
-          console.error(e)
-        })
-    }, 3000)
-
-    return () => {
-      clearInterval(id)
     }
-  }, [account, investStatus, pageNum, pageSize])
+  )
+
   return {
     orderList,
     pageParams
