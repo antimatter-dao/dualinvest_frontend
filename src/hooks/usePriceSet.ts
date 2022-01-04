4ad51797fd7350bdd1e724556670be522356a97f
iv172 <iv172@hotmail.com>
2022-01-04 13:33:49 +0800

    fix usePrice delay

--- src/hooks/usePriceSet.ts
+++ src/hooks/usePriceSet.ts
@@ -42,7 +42,7 @@ export function usePriceSet(symbol: string | undefined) {
   return priceSet
 }
 
-export function usePrice(symbol: string | undefined, delay = 3000) {
+export function usePrice(symbol: string | undefined, delay = 5000) {
   const [price, setPrice] = useState<undefined | string>(undefined)
 
   useEffect(() => {
