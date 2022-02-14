279cfea948c7048c68eee7d5cddee1161f631d29
iv172 <iv172@hotmail.com>
2022-02-14 19:33:16 +0800

    fix history filter

--- src/hooks/useAccountData.ts
+++ src/hooks/useAccountData.ts
@@ -76,9 +76,14 @@ export function useOrderRecords(
       investType,
       investStatus: Array.isArray(investStatus) ? undefined : investStatus,
       pageNum: Array.isArray(investStatus) ? undefined : pageNum,
-      pageSize: Array.isArray(investStatus) ? undefined : pageSize
+      pageSize: Array.isArray(investStatus) ? undefined : pageSize,
+      ...(currency === 'All'
+        ? {}
+        : {
+            currency: currency
+          })
     })
-  }, [account, investStatus, investType, pageNum, pageSize])
+  }, [account, currency, investStatus, investType, pageNum, pageSize])
 
   const callbackFn = useCallback(r => {
     setOrderList(r.data.data.records)
